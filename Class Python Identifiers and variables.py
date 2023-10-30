0155b4299fa019064f71fcf3d60bf89028d7af5a
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2023-10-30 17:57:50 +0600

    testing

---
+++ Class Python Identifiers and variables.py
@@ -0,0 +1,27 @@
+#one
+"""
+_hello = 7
+hello = 8
+_he8_he = 10
+"""
+# but '9he_ = 10' idetifiers cannot start with  numbers
+"""
+hello=5
+Hello=5
+#here Hello is H sensitive
+"""
+#two
+#cannot use keyword for naming identifiers
+#it is not possible 'def = 10'
+#An identifier is a name given to a programming construct,
+# while a variable is a specific type of identifier used
+# to store and manage data in a program.
+
+"""
+hello=4
+hello=7
+#here hello is a variable
+"""
+
+msg = 'c:\python\bin'
+print(msg)
